1a7b1b8ed8f2918b5b721eaeabd9fed913668540
rifanabiyoga391 <rifanabiyoga391@gmail.com>
2023-01-07 00:48:05 -0800

    master : menmbahkan controler data_barang.php dan model_barnag.php

--- application/models/model_barang.php
+++ application/models/model_barang.php
@@ -4,4 +4,7 @@ class model_barang extends CI_Model{
     public function tampil_data(){
         return $this->db->get('tb_barang');
     }
+    public function tambah_barang($data,$table){
+        $this->db->insert($table,$data);
+    }
 }
