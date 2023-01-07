580df5c7c03f8fad12b3e8b9690363c771fccf34
PratamaBudiRusadi <pratamabudi455#gmail.com>
2023-01-07 00:18:25 -0800

    pratama : menambahkan form input produk di data_barang.php

--- application/views/admin/data_barang.php
+++ application/views/admin/data_barang.php
@@ -36,3 +36,57 @@
 
     </table>
 </div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="tambah_barang" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Form Input Produk</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="<?php echo base_url(). 'admin/data_barang/tambah_aksi' ?>" method="post" enctype="multipart/form-data">
+        <div class="form group">
+             <label>Nama Barang</label>
+             <input type="text" name="nama_brg" class="form-control">
+        </div>
+        <div class="form group">
+             <label>Keterangan</label>
+             <input type="text" name="keterangan" class="form-control">
+        </div>
+        <div class="form group">
+             <label>Kategori</label>
+             <select class="form-control" name="kategori">
+              <option>Oli</option>
+              <option>Filter Oli</option>
+              <option>Ban Luar</option>
+              <option>Kampas Rem</option>
+             </select>
+        </div>
+        <div class="form group">
+             <label>Harga</label>
+             <input type="text" name="harga" class="form-control">
+        </div>
+        <div class="form group">
+             <label>Stock</label>
+             <input type="text" name="stok" class="form-control">
+        </div>
+        <div class="form group">
+             <label>Gambar Produk</label><br>
+             <input type="file" name="gambar" class="form-control">
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Simpan</button>
+      </div>
+
+      </form>
+
+    </div>
+  </div>
+</div>
