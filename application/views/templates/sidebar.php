32c661a745ec82b481d7969642632839d46200e5
PratamaBudiRusadi <pratamabudi455@gmail.com>
2023-01-12 10:41:43 -0800

    paratama : perbaikan di view templates sidebar.php

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -131,7 +131,7 @@
                                     $keranjang = 'Keranjang Belanja: ' .$this->cart->total_items
                                     (). 'items' 
                                     ?>
-                                    <?php echo $keranjang  ?>
+                                    <?php echo anchor('dashboard/detail_keranjang', $keranjang)  ?>
                                 </li>
                             </ul>
 
